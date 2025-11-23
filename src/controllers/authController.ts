9453d4847b1e95be8cfc4c838ff9d4ce4b25af8e
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2025-11-23 11:56:31 -0800

    register & login logic

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express'
 import { db } from '../db/connection.ts'
 import { users , type NewUser, type User} from '../db/schema.ts'
 import { generateToken } from '../utils/jwt.ts'
-import { hashPassword } from '../utils/passwords.ts'
+import { comparePassword, hashPassword } from '../utils/passwords.ts'
 import { eq } from 'drizzle-orm'
 
 
@@ -43,4 +43,46 @@ export const register  = async (req: Request<any, any , NewUser>, res: Response)
         res.status(500).json({error: 'Failed to create user'})
     }
 
+}
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        
+        const { email, password } = req.body;
+        //find first (if broke res = 500)
+        const user = await db.query.users.findFirst({
+            where:  eq(users.email, email)
+        })
+        if (!user) {
+            return res.status(401).json({error: 'Invalid credentials'})
+        }
+         //compare password (if broke res = 500)
+        const isValidatedPassword = await comparePassword(password, user.password)
+
+        if (!isValidatedPassword) {
+            return res.status(401).json({error: 'Invalid credentials'})
+        }
+         //token generation (if broke res = 500)
+        const token = await generateToken({
+            id: user.id,
+            email: user.email,
+            username: user.username
+        })
+
+        return res.status(201).json({
+            message: 'Login success',
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                createdAt: user.createdAt,
+            },
+           token,
+        })
+    } catch(e) {
+     console.error('Login error', e)
+        res.status(500).json({ error: 'Failed to login'})
+    }
 }
--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -5,3 +5,7 @@ import evn from '../../env.ts'
 export const hashPassword = async (password: string) => {
     return bcrypt.hash(password, evn.BCRYPT_ROUNDS)
 }
+
+export const comparePassword = async (password: string, hashedPassword: string) => {
+    return bcrypt.compare(password, hashedPassword)
+}
